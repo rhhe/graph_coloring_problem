d1cbd195872c6333d6564addf7005c360a06332e
Ron <ronghuahe2007@163.com>
2023-08-09 21:11:48 +0800

    count conflict.

--- test.cpp
+++ test.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-void testRandomColor() {
+void TestRandomColor() {
     Graph graph;
     graph.ReadFromFile("../DSJC500.5.col");
     graph.PreprocessMultiModeDataStruct();
@@ -17,7 +17,20 @@ void testRandomColor() {
     assert(allColor.size() == k);
 }
 
+void TestConflictCount() {
+    Graph graph;
+    graph.ReadFromFile("../DSJC500.5.col");
+    graph.PreprocessMultiModeDataStruct();
+    int k = 1;
+    int seed = 1999;
+    auto colors = Algorithm::ColorRandomly(graph, k, seed);
+    auto adjacentColorTable = Algorithm::MakeAdjacentColorCountTable(graph, colors, k);
+    int nConflict = Algorithm::CountConflictNumWithAdjacentTable(colors, adjacentColorTable);
+    assert(nConflict == graph.nEdge_);
+}
+
 int main() {
-    testRandomColor();
+    TestRandomColor();
+    TestConflictCount();
     return 0;
 }
